c6263d84bbf567a22a8c9b94a5da1ac1c59fb58d
Aminata <dramesene1509@gmail.com>
2020-11-07 15:31:55 +0000

    add login, main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { DetailsComponent } from './details/details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
